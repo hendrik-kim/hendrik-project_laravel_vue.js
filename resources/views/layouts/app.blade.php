569e61c6cd7aad50db8f68e75d8abb6dfb03c180
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-25 09:12:38 -0600

    register paged added

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+   <title>Laravel</title>
+ </head>
+ <body class='bg-gray-200'>
+     <nav class="p-6 bg-white flex justify-between mb-6">
+         <ul class="flex items-center">
+             <li>
+                 <a class="p-3" href="">Home</a>
+             </li>
+             <li>
+                 <a class="p-3" href="">Dashboard</a>
+             </li>
+             <li>
+                 <a class="p-3" href="">Post</a>
+             </li>
+         </ul>
+         <ul class="flex items-center">
+            <li>
+                <a class="p-3" href="">Hendrik</a>
+            </li>
+            <li>
+                <a class="p-3" href="">Login</a>
+            </li>
+            <li>
+                <a class="p-3" href="">Logout</a>
+            </li>
+            <li>
+                <a class="p-3" href="{{route('register')}}">Register</a>
+            </li>
+        </ul>
+     </nav>
+   @yield('content')
+ </body>
+ </html>
