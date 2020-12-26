50c3a4575106c8ca1723d0ba2e6723a8d561f8a7
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-26 00:31:09 -0600

    'init'

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
-// Route::get('/register', [RegisterController::class, 'store']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::post('/register', [RegisterController::class, 'store']);
-
-Route::get('/posts', function () {
-    return view('posts.index');
+Route::get('/', function () {
+    return view('welcome');
 });
