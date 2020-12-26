953642eeb3c65b6f79ea156b2e56f18b56e37c6b
hank.dohwankim <hank.dohwankim@gmail.com>
2020-12-26 02:34:02 -0600

    Styling the navigation bar

--- resources/js/Index.vue
+++ resources/js/Index.vue
@@ -1,10 +1,16 @@
 <template>
     <div>
         <nav class="navbar bg-white border-bottom navbar-light">
-            <router-link :to="{ name: 'home' }">LarabelBnb</router-link>
-            <router-link :to="{ name: 'example' }">2nd</router-link>
+            <router-link class="navbar-brand mr-auto" :to="{ name: 'home' }"
+                >LarabelBnb</router-link
+            >
+            <router-link class="btn nav-buttom" :to="{ name: 'example' }"
+                >2nd</router-link
+            >
         </nav>
-        <router-view></router-view>
+        <div class="container mt-4 mb-4 pr-4 pl-4">
+            <router-view></router-view>
+        </div>
     </div>
 </template>
 
