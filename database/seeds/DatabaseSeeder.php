aae05f0f4c6cc41f76637aaed2cda3ea5a01f80f
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2021-01-10 01:19:13 -0600

    initial project

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,18 +1,16 @@
 <?php
 
-namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // $this->call(UsersTableSeeder::class);
     }
 }
